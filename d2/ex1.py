75f4a7c0ee9102a137c23c30005575820e84f92d
kiki7766 <peggy.gonzalez@upr.edu>
2024-04-21 22:47:21 -0400

    Adding all stuff

---
+++ d2/ex1.py
@@ -0,0 +1,20 @@
+# print(len("hello"))
+
+# Data Types
+
+# String "Hello"
+print("Hello"[4])
+
+# Float 
+print(3.14159)
+
+# Integer
+print("123" + "345")
+
+#123_456_789
+
+#Boolean
+# True or False
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
---
+++ d2/ex2.py
@@ -0,0 +1,5 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+str(two_digit_number)
