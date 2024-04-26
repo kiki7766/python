81a747a306cb6f0aed3ac7ae0a14747e10f1d6b4
kiki7766 <peggy.gonzalez@upr.edu>
2024-04-26 15:04:23 -0400

    adding solution to problem

--- d2/ex2.py
+++ d2/ex2.py
@@ -1,5 +1,20 @@
+# Instructions
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+# Warning. Do not change the code on line 1. Your program should work for different inputs. e.g. any two-digit number.
+
+# The last line of your program should print the result.
+
+# Example 1 Input
+# 39
+
+# Example 1 Output
+# 12
+
 two_digit_number = input()
 # 🚨 Don't change the code above 👆
 ####################################
 # Write your code below this line 👇
-str(two_digit_number)
+change = str(two_digit_number)
+print(int(change[0]) + int(change[1]))
+
